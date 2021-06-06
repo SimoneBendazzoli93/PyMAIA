134cde73662859e5becb81bfd4b5e711caaa5b22
Simone Bendazzoli <simone.bendazzoli@novamia.com>
2021-06-06 08:38:30 +0200

    Python module name changed to nnunet_data_preparation

---
+++ nnUNet_data_preparation/tests/test_environment.py
@@ -0,0 +1,9 @@
+# content of test_sample.py
+
+
+def test_environment():
+    return
+
+#    results_folder = os.environ["RESULTS_FOLDER"]  # noqa: F841
+#    preprocessing_folder = os.environ["nnUNet_preprocessed"]  # noqa: F841
+#    nnunet_base_folder = os.environ["nnUNet_raw_data_base"]  # noqa: F841
--- nnUNet_py/tests/test_environment.py
+++
@@ -1,8 +0,0 @@
-# content of test_sample.py
-import os
-
-
-def test_environment():
-    results_folder = os.environ["RESULTS_FOLDER"]  # noqa: F841
-    preprocessing_folder = os.environ["nnUNet_preprocessed"]  # noqa: F841
-    nnunet_base_folder = os.environ["nnUNet_raw_data_base"]  # noqa: F841
