d92ae2cc7d12fb21a4446427503f1f9d66a6d501
Simone Bendazzoli <simonebendazzoli93@gmail.com>
2021-06-10 11:30:53 +0200

    setup.py requirements updated

--- k8s_data_preparation/setup.py
+++ k8s_data_preparation/setup.py
@@ -4,22 +4,18 @@ import setuptools
 from setuptools import setup
 
 setup(
-    name="nnUNet_data_preparation",
+    name="nnunet_data_preparation",
     version="1.0",
     url="https://github.com/SimoneBendazzoli93/k8s_nnUNet.git",
     license="",
     author="Simone Bendazzoli",
     author_email="simben@kth.se",
-    description="Tool to standardize dataset folder to match nnUNet folder structure",
+    description="Tool to standardize dataset folder to match nnUNet folder structure",  # noqa: E501
     packages=setuptools.find_packages("src"),
     package_data={
         "": ["*.yml"],
     },
     package_dir={"": "src"},
-    install_requires=[
-        "setuptools",
-        "coloredlogs",
-        "argparse",
-    ],
+    install_requires=["coloredlogs", "numpy", "SimpleITK"],
     scripts=glob.glob("scripts/*"),
 )
