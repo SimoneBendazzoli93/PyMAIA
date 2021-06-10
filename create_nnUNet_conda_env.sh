03741377a156935514be1b6d952da3fa17f0acd0
Simone Bendazzoli <simonebendazzoli93@gmail.com>
2021-06-10 11:55:45 +0200

    Python package name changed to nnunet_data_preparation

--- create_nnUNet_conda_env.sh
+++ create_nnUNet_conda_env.sh
@@ -3,5 +3,5 @@ conda init bash
 conda config --set auto_activate_base true
 conda create -n nnUNet --clone base
 conda activate nnUNet
-conda install -c conda-forge --file /workspace/conda_requirements.txt -y
+pip install /workspace/conda_requirements.txt -y
 conda env config vars set $(eval echo $(cat /workspace/.env))
--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 echo $user
 groupadd --gid 1111 docker_user_group
 useradd -m -s /bin/bash -u 1000 --groups docker_user_group $user
-echo $user":"$pw | chpasswd
+echo $user":"$password | chpasswd
 adduser $user sudo
 
 cd /home/$user
@@ -11,5 +11,5 @@ cd /home/$user
 su $user "/workspace/create_nnUNet_conda_env.sh"
 
 ln -s /home/user/LungLobeSeg /home/$user/LungLobeSeg
-ln -s /home/user/nnunet_py /home/$user/nnunet_py
+ln -s /home/user/nnunet_data_preparation /home/$user/nnunet_data_preparation
 exec "$@"
