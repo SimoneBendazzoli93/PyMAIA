991c4d6e102852c81da3e68da652a793db7eeffa
SimoneBendazzoli93 <simone.bendazzoli@novamia.com>
2021-07-20 18:19:09 +0200

    GIT_SSH_COMMAND in conda env

--- create_nnUNet_conda_env.sh
+++ create_nnUNet_conda_env.sh
@@ -5,5 +5,5 @@ conda create -n nnUNet --clone base
 conda activate nnUNet
 pip install -r /workspace/conda_requirements.txt
 conda env config vars set $(eval echo $(cat /workspace/.env))
-export GIT_SSH_COMMAND="ssh -v -i /etc/secret-volume/ssh-privatekey"
+conda env config vars set GIT_SSH_COMMAND="ssh -v -i /etc/secret-volume/ssh-privatekey"
 yes | pip install git+ssh://git@github.com/SimoneBendazzoli93/k8s_nnUNet.git
