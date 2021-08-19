2e4f5d185c039f103c090361c6598053cc6cf61f
SimoneBendazzoli93 <simone.bendazzoli@novamia.com>
2021-08-19 17:36:56 +0200

    Docker folder moved to nnUNet

--- docker/nnUNet/create_nnUNet_conda_env.sh
+++ docker/nnUNet/create_nnUNet_conda_env.sh
@@ -6,5 +6,5 @@ conda activate nnUNet
 pip install -r /workspace/conda_requirements.txt
 conda env config vars set $(eval echo $(cat /workspace/.env))
 conda env config vars set GIT_SSH_COMMAND="ssh -v -i /etc/secret-volume/ssh-privatekey"
-pip install git+https://github.com/SimoneBendazzoli93/Hive.git@v1.0
-#pip install git+ssh://git@github.com/SimoneBendazzoli93/Hive.git@v1.0
+pip install git+https://github.com/SimoneBendazzoli93/Hive.git@v1.1
+#pip install git+ssh://git@github.com/SimoneBendazzoli93/Hive.git@v1.1
