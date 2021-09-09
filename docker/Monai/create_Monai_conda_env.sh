18e0e20dfc73a5f98ebecb4acf6c7f5f1188a86c
Simone Bendazzoli <simonebendazzoli93@gmail.com>
2021-09-09 09:37:37 +0200

    conda env cloned from base

--- docker/Monai/create_Monai_conda_env.sh
+++ docker/Monai/create_Monai_conda_env.sh
@@ -1,7 +1,7 @@
 source /opt/conda/bin/activate
 conda init bash
 conda config --set auto_activate_base true
-conda create -n Monai python=3.8 -y
+conda create -n Monai --clone base -y
 
 echo 'conda activate Monai' >> /home/$user/.bashrc
 
--- docker/nnUNet/create_nnUNet_conda_env.sh
+++ docker/nnUNet/create_nnUNet_conda_env.sh
@@ -1,6 +1,6 @@
 source /opt/conda/bin/activate
 conda init bash
 conda config --set auto_activate_base true
-conda create -n nnUNet python=3.8 -y
+conda create -n nnUNet --clone base -y
 
 echo 'conda activate nnUNet' >> /home/$user/.bashrc
