f3af0349ee006c7d599c7c84dba0cbd3dca34098
Simone Bendazzoli <simonebendazzoli93@gmail.com>
2021-10-21 16:44:24 +0200

    Preprocessing transform added

--- src/Hive/monai/transforms/__init__.py
+++ src/Hive/monai/transforms/__init__.py
@@ -2,4 +2,5 @@ from .io.dictionary import Save2DSlicesd  # NOQA: F401
 from .matrix_orientation.dictionary import OrientToRAId  # NOQA: F401
 from .post.array import ToListMemorySaver, ThresholdForegroundLabels  # NOQA: F401
 from .post.dictionary import ThresholdForegroundLabelsd  # NOQA: F401
+from .preprocessing.dictionary import LungLobeMapToFissureMaskd  # NOQA: F401
 from .utils import get_axis_order_to_RAI, get_quatern_a  # NOQA: F401
