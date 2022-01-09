1f9a37f789a6398b36633b885c72bb8994a366e4
SimoneBendazzoli93 <simone.bendazzoli@novamia.com>
2022-01-09 09:05:30 +0100

    receiver_email added

--- docker/Monai/start.sh
+++ docker/Monai/start.sh
@@ -7,6 +7,7 @@ adduser $user sudo
 su $user "/workspace/create_Monai_conda_env.sh"
 
 echo 'source /workspace/.env' >> /home/$user/.bashrc
+echo 'export receiver_email='$email >> /home/$user/.bashrc
 #pip install git+https://github.com/SimoneBendazzoli93/Hive.git@v1.1
 #pip install git+ssh://git@github.com/SimoneBendazzoli93/Hive.git@v1.1
 exec "$@"
