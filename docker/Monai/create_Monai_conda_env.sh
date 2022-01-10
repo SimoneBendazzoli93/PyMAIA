93b6ab5f5401045c6c6774f2a445c08e1c2dff13
SimoneBendazzoli93 <simone.bendazzoli@novamia.com>
2022-01-10 10:09:01 +0100

    MONAI docker and helm chart removed

--- docker/Monai/create_Monai_conda_env.sh
+++
@@ -1,7 +0,0 @@
-source /opt/conda/bin/activate
-conda init bash
-conda config --set auto_activate_base true
-conda create -n Monai --clone base -y
-
-echo 'conda activate Monai' >> /home/$user/.bashrc
-
