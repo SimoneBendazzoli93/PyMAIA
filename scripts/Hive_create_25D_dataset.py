7e957280053a8d4577b7524a032fda5c4c7e358c
SimoneBendazzoli93 <simone.bendazzoli@novamia.com>
2022-01-27 09:21:02 +0100

    Update 2D slicing

--- scripts/Hive_create_25D_dataset.py
+++ scripts/Hive_create_25D_dataset.py
@@ -2,26 +2,26 @@
 
 import datetime
 import json
-import os
+from multiprocessing import Pool
 from argparse import ArgumentParser, RawTextHelpFormatter
 from pathlib import Path
 from textwrap import dedent
 
-from Hive.monai.apps.datasets import LungLobeDataset
+from Hive.monai.apps.datasets import LungLobeDataset, CrossValidationDataset
 from Hive.monai.transforms import OrientToRAId, Save2DSlicesd
 from Hive.utils.log_utils import add_verbosity_options_to_argparser, get_logger, log_lvl_from_verbosity_args
-from monai.apps import CrossValidation
-from monai.transforms import LoadImaged, Compose
-from tqdm import tqdm
 
+from monai.transforms import LoadImaged, Compose, Resize
+from tqdm import tqdm
+from Hive.utils.file_utils import generate_dataset_json
 TIMESTAMP = "{:%Y-%m-%d_%H-%M-%S}".format(datetime.datetime.now())
 
 DESC = dedent(
     """
     Preprocess a dataset for a 2.5D experiment. A 3D dataset is sliced along the specified directions ( [``axial``,
-    ``coronal``, ``sagittal``] ) and the set of 2D slices are stored as PNG, Numpy (*.npy*) or compressed Numpy
-    (*.npz*) files. For each fold and each orientation, a JSON configuration file is also saved, which can be used in later
-    training stages in the Monai environment.
+    ``coronal``, ``sagittal``] ) and the set of 2D slices are stored as PNG,NIFTI (*.nii.gz*), Numpy (*.npy*) or compressed Numpy
+    (*.npz*) files. For each fold and each orientation, a JSON dataset summary file is also saved, which can be used in later
+    training stages.
     """  # noqa: E501
 )
 EPILOG = dedent(
@@ -80,15 +80,14 @@ def main():
     with open(arguments["config_file"]) as json_file:
         config_dict = json.load(json_file)
 
-    preprocessing_path = config_dict["preprocessing_folder"]
-    orientations = list(config_dict["slice_size_2d"].keys())
+    orientations = config_dict["orientations"]
     n_fold = config_dict["n_folds"]
     slice_2d_extension = config_dict["save_2D_slices_as"]
     file_extension = config_dict["FileExtension"]
-    random_seed = config_dict["Seed"]
+    random_seed = 12345#config_dict["Seed"]
     n_workers = arguments["n_workers"]
     n_cache = arguments["n_cache"]
-    dataset_folder = Path(config_dict["base_folder"]).joinpath(
+    dataset_folder = Path(config_dict["base_folder"]).joinpath("nnUNet_raw_data",
         "Task" + config_dict["Task_ID"] + "_" + config_dict["Task_Name"])
     transforms = Compose(
         [
@@ -96,7 +95,8 @@ def main():
             OrientToRAId(keys=["image", "label"]),
             Save2DSlicesd(
                 keys=["image", "label"],
-                output_folder=preprocessing_path,
+                output_folder=str(dataset_folder),
+                rescale_to_png={"image": True,"label":True},
                 file_extension=file_extension,
                 slicing_axes=orientations,
                 slices_2d_filetype=slice_2d_extension,
@@ -104,56 +104,42 @@ def main():
         ]
     )
 
-    cv_dataset = CrossValidation(
+    cv_dataset = CrossValidationDataset(
         dataset_cls=LungLobeDataset,
         nfolds=n_fold,
         seed=random_seed,
         dataset_dir=dataset_folder,
         section="training",
-        transform=transforms,
         num_workers=n_workers,
         cache_num=n_cache,
     )
 
-    for fold in tqdm(range(n_fold), desc="Preprocessing Folds"):
+    training_folds = list(range(n_fold))
 
-        training_folds = list(range(n_fold))
-        training_folds.remove(fold)
-        dataset_fold_train = cv_dataset.get_dataset(folds=training_folds)
-        dataset_fold_val = cv_dataset.get_dataset(folds=fold)
+    dataset = cv_dataset.get_dataset(folds=training_folds)
+    pool = Pool(n_workers)
+    transformed_data = []
+    for data in dataset:
+        transformed_data.append(
+                (
+                    data
+                ),
+            )
 
-        section_dict = {
-            "validation": dataset_fold_val,
-            "training": dataset_fold_train,
-        }
+    res = pool.starmap_async(transforms, transformed_data)
+    pool.close()
 
-        for section in section_dict:
-            dataset = section_dict[section]
-            for orientation in orientations:
-                filename_dict = [
-                    {**image_filename, **label_filename}
-                    for data in dataset
-                    for image_filename, label_filename in zip(
-                        data["image_meta_dict"]["filenames_{}".format(orientation)],
-                        data["label_meta_dict"]["filenames_{}".format(orientation)],
-                    )
-                ]
-                config_dict = {
-                    "Fold": fold,
-                    "orientation": orientation,
-                    "{}_2D".format(section): filename_dict,
-                    "num{}_2D".format(section.capitalize()): len(filename_dict),
-                    "{}_3D".format(section): dataset.data,
-                    "num{}_3D".format(section.capitalize()): len(dataset.data),
-                }
+    [res.get() for _ in tqdm(transformed_data, desc="2D Slicing")]
+    pool.join()
 
-                config_filepath = os.path.join(
-                    preprocessing_path, orientation, "data",
-                    "dataset_fold_{}_{}_{}.json".format(fold, section, orientation)
-                )
-
-                json_file = open(config_filepath, "w")
-                json.dump(config_dict, json_file)
+    for orientation in orientations:
+        generate_dataset_json(str(Path(dataset_folder).joinpath(orientation,"dataset.json")),
+                            str(Path(dataset_folder).joinpath(orientation,"imagesTr")),
+                            None,
+                            list(config_dict["Modalities"].values()),
+                            config_dict["label_dict"],
+                            config_dict["DatasetName"]+"_2D_{}".format(orientation),
+                            )
 
 
 if __name__ == "__main__":
