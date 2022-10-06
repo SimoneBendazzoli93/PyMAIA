94ce3dfd30e46f91de63515488e0fc8f320e4d9d
SimoneBendazzoli93 <simone.bendazzoli@novamia.com>
2022-10-06 17:30:43 +0200

    nndet_prepare_data_folder script added

--- setup.py
+++ setup.py
@@ -5,34 +5,19 @@ from setuptools import setup
 
 setup(
     name="Hive",
-    version="2.0",
+    version="2.1",
     url="https://github.com/SimoneBendazzoli93/Hive.git",
-    license="",
+    license="GPLv3",
     author="Simone Bendazzoli",
     author_email="simben@kth.se",
-    description="",  # noqa: E501
+    description="Python Package to support Deep Learning data preparation, pre-processing. training, result visualization"
+                " and model deployment across different frameworks (nnUNet, nnDetection, MONAI).",
     packages=setuptools.find_packages("src"),
     package_data={
         "": ["configs/*.yml", "configs/*.json"],
     },
     package_dir={"": "src"},
-    install_requires=[
-        "visdom",
-        "coloredlogs",
-        "numpy",
-        "nptyping",
-        "SimpleITK",
-        "pandas",
-        "scipy",
-        "tqdm",
-        "MedPy",
-        "plotly",
-        "kaleido",
-        "nbformat",
-        "pandasgui",
-        "xlsxwriter",
-        "openpyxl",
-    ],
-    extras_require={"Monai-env": ["monai", "pytorch-ignite"], "nnUNet-env": ["nnunet"]},
+    install_requires=["tqdm", "coloredlogs", "dicom2nifti", "nibabel", "scikit_learn", "numpy", "pydicom", "pynvml",
+                      "SimpleITK"],
     scripts=glob.glob("scripts/*"),
 )
