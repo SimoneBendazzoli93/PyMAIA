6f7ebde9b49618b76c77b6a0888b133b02cfc976
SimoneBendazzoli93 <simone.bendazzoli@novamia.com>
2022-11-29 17:38:59 +0100

    Setup Updated

--- setup.py
+++ setup.py
@@ -23,14 +23,19 @@ def read_file(file):
 
 
 setup(
-    name="Hive",
+    name="hive-maia",
     version="1.0",
     url="https://github.com/MAIA-KTH/Hive.git",
     license="GPLv3",
+    project_urls={
+        'Documentation': 'https://hive-maia.readthedocs.io',
+        'Source': 'https://github.com/MAIA-KTH/Hive',
+        'Tracker': 'https://github.com/MAIA-KTH/Hive/issues',
+    },
     author="Simone Bendazzoli",
     author_email="simben@kth.se",
     description="Python Package to support Deep Learning data preparation, pre-processing. training, result visualization"
-    " and model deployment across different frameworks (nnUNet, nnDetection, MONAI).",
+                " and model deployment across different frameworks (nnUNet, nnDetection, MONAI).",
     long_description=read_file(os.path.join(os.path.dirname(__file__), "README.md")),
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
@@ -41,15 +46,16 @@ setup(
     install_requires=resolve_requirements(os.path.join(os.path.dirname(__file__), "requirements.txt")),
     entry_points={
         "console_scripts": [
-            "Hive_convert_DICOM_dataset_to_NIFTI_dataset = scripts.Hive_convert_DICOM_dataset_to_NIFTI_dataset:main",
-            "Hive_run_pipeline_from_file = scripts.Hive_run_pipeline_from_file:main",
-            "Hive_create_subset = scripts.Hive_create_subset:main",
-            "Hive_convert_semantic_to_instance_segmentation = scripts.Hive_convert_semantic_to_instance_segmentation:main",
-            "nndet_create_pipeline = scripts.nndet_create_pipeline:main",
-            "nndet_prepare_data_folder = scripts.nndet_prepare_data_folder:main",
-            "nndet_run_preprocessing = scripts.nndet_run_preprocessing:main",
-            "nndet_run_training = scripts.nndet_run_training:main",
-            "Hive_convert_semantic_to_instance_segmentation = scripts.Hive_convert_semantic_to_instance_segmentation:main",
+            "Hive_convert_DICOM_dataset_to_NIFTI_dataset = Hive_scripts.Hive_convert_DICOM_dataset_to_NIFTI_dataset:main",
+            "Hive_run_pipeline_from_file = Hive_scripts.Hive_run_pipeline_from_file:main",
+            "Hive_create_subset = Hive_scripts.Hive_create_subset:main",
+            "nndet_create_pipeline = Hive_scripts.nndet_create_pipeline:main",
+            "nndet_prepare_data_folder = Hive_scripts.nndet_prepare_data_folder:main",
+            "nndet_run_preprocessing = Hive_scripts.nndet_run_preprocessing:main",
+            "nndet_run_training = Hive_scripts.nndet_run_training:main",
+            "Hive_convert_semantic_to_instance_segmentation = Hive_scripts.Hive_convert_semantic_to_instance_segmentation:main",
+            "Hive_extract_experiment_predictions = Hive_scripts.Hive_extract_experiment_predictions:main",
+            "Hive_order_data_folder = Hive_scripts.Hive_order_data_folder:main"
         ],
     },
     keywords=["deep learning", "image segmentation", "medical image analysis", "medical image segmentation"],
