4fa44a227528e24aef4eb39ae77fdf1ab42179d1
Simone Bendazzoli <simben@kth.se>
2023-04-19 15:52:24 +0200

    Pipeline step added for final Sweep and Consolidation

--- Hive_scripts/nndet_create_pipeline.py
+++ Hive_scripts/nndet_create_pipeline.py
@@ -164,7 +164,10 @@ def main():
 
     [pipeline_steps.append(step) for step in run_training_step(output_json_config_file, range(config_dict["n_folds"]))]
 
-    Path(os.environ["root_experiment_folder"]).joinpath(config_dict["Experiment Name"]).mkdir(exist_ok=True, parents=True)
+    pipeline_steps.append(run_training_step(output_json_config_file, "-1"))
+
+    Path(os.environ["root_experiment_folder"]).joinpath(config_dict["Experiment Name"]).mkdir(exist_ok=True,
+                                                                                              parents=True)
     pipeline_steps_summary = open(
         Path(os.environ["root_experiment_folder"]).joinpath(
             config_dict["Experiment Name"], "Task_" + arguments["task_ID"] + "_" + TIMESTAMP + ".txt"
